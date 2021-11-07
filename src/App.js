d0cd261656bb938f194d3b984ca886355c71b336
SPARTACUS5329 <adityaspartacus5329@gmail.com>
2021-11-07 20:07:01 +0530

    made minimal to the problem

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import { useState, useEffect } from "react";
 import generateRandomArr from "./utils/randomArrayGenerator";
-import bubbleSort from "./utils/bubbleSort";
 import mergeSort from "./utils/mergedSort";
-import insertionSort from "./utils/insertionSort";
-import selectionSort from "./utils/selectionSort";
 import Bar from "./Bar";
 import "./styles/App.css";
 
@@ -14,18 +11,6 @@ function App() {
 
   const generateNewArray = () => {
     setArray(generateRandomArr(5, 650, ARRAY_SIZE));
-  };
-
-  const bubbleSortArray = async () => {
-    console.log(await bubbleSort([...array], setArray));
-  };
-
-  const insertionSortArray = async () => {
-    setArray(await insertionSort([...array], setArray));
-  };
-
-  const selectionSortArray = async () => {
-    setArray(await selectionSort([...array], setArray));
   };
 
   const mergedSortArray = async () => {
@@ -42,10 +27,7 @@ function App() {
   return (
     <div className="App">
       <button onClick={generateNewArray}>Generate New Array</button>
-      <button onClick={bubbleSortArray}>Bubble Sort</button>
       <button onClick={mergedSortArray}>Merge Sort</button>
-      <button onClick={insertionSortArray}>Insertion Sort</button>
-      <button onClick={selectionSortArray}>Selection Sort</button>
 
       <div className="containter">
         <ul>
